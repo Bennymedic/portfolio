deaa969da877e03629b88b38846d6f58ca8456ae
Benny Medic <beniyambrhane00@gmail.com>
2024-07-28 16:51:38 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+  ## Beniyam's Portfolio
---
+++ src/constants/index.js
@@ -0,0 +1,260 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+import angular from "../assets/tech/angular.png"
+import sweven from "../assets/company/sweven.jpg"
+import freelancer from "../assets/company/freelancer.jpg"
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "angular",
+    icon: angular,
+  },
+  // {
+  //   name: "Redux Toolkit",
+  //   icon: redux,
+  // },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+ 
+];
+
+const experiences = [
+  {
+    title: "Software Developer",
+    company_name: "Freelancer",
+    icon: freelancer,
+    iconBg: "#383E56",
+    date: "Jun 2024 - Present",
+    points: [
+      "Developed reusable Angular components, leading an increase in code reuse and consistency across the application.",
+      "Created independent, self-contained front-end modules for separate development, testing, and deployment.",
+      "Optimized software components, modules, and algorithms for high performance and accuracy.",
+      "Leveraged Redux for state management, ensuring predictable state transitions and enhancing the application's scalability.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Sweven Trading PLC",
+    icon: sweven,
+    iconBg: "#E6DEDD",
+    date: "Oct 2020 - Jun 2023",
+    points: [
+      "Led a cross-functional team of 3 developers, ensuring on-time project completion and conducting regular consulting knowledge-sharing sessions to enhance workflow efficiency.",
+      "Incorporated core and recent React features to encapsulate reusable logic, enhancing code readability and reducing redundancy across the codebase.",
+      "Created independent, self-contained front-end modules for separate development, testing, and deployment, reducing integration issues ",
+      "Participated in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Junior Software Developer",
+    company_name: "Sweven Trading PLC",
+    icon: sweven,
+    iconBg: "#E6DEDD",
+    date: "Aug 2018 - Oct 2020",
+    points: [
+      "Developed RESTful services and middleware with Express.js, ensuring robust error handling and response.",
+      "Collaborated closely with UX/UI designers, translating wireframes and prototypes into interactive and responsive user interfaces.",
+      "Implemented responsive design and ensuring cross-browser compatibility.",
+      "Executed comprehensive testing with Jest, Mocha, and Cypress",
+    ],
+  },
+  
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
